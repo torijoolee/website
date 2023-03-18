61754f088935c89152958560d2880b7d8855efff
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-18 15:54:43 +0900

    qrcode

--- js/modal-close.js
+++ js/modal-close.js
@@ -1,10 +1,43 @@
-// (() => {
-//   const modalClose = document.querySelector(".close-btn");
-//   const modal = document.querySelector(".design input");
+(() => {
+  const modalClose = document.querySelector(".close-btn");
+  const input = document.querySelector(".design input");
+  let checked = true;
 
-//   modalClose.addEventListener("click", function (e) {
-//     if (e.target.checked == true) {
-//       e.target.checked = false;
-//     }
-//   });
-// })();
+  modalClose.addEventListener("click", function (e) {
+    if (e.target.checked == true) {
+      e.target.checked = false;
+    }
+  });
+})();
+(() => {
+  const modal = document.querySelectorAll(".design .modal");
+  const designCon = document.querySelector(".design-con");
+  let mouseStart = 0;
+  let move = 0;
+  let click = false;
+
+  designCon.addEventListener("mousedown", function (e) {
+    mouseStart = e.clientX;
+    click = true;
+    // if (click == true) {
+    //   modal.forEach((item) => {
+    //     item.style.display = "none";
+    //   });
+    // }
+  });
+
+  //check box가 선택되면
+  designCon.addEventListener("mousemove", function (e) {
+    if (click == true) {
+      console.log(e.clientX - mouseStart);
+      move = e.clientX - mouseStart;
+      designCon.style.transform = `translateX(${move}px)`;
+      // modal.forEach((item) => {
+      //   item.style.display = "none";
+      // });
+    }
+  });
+  designCon.addEventListener("mouseup", function (e) {
+    click = false;
+  });
+})();
